8c9c14fbd45237d1651020ec5adf6253cd613258
Egmara <egmara.antunes@gmail.com>
2019-07-15 17:36:44 -0300

    gera alguns resultados

--- gera_resultados.py
+++ gera_resultados.py
@@ -29,7 +29,7 @@ resultados = classifica_teste('testes/teste4.jpeg', modelos)
 print(f'Rede Neural: {resultados[0]}, \nFloresta: {resultados[1]}')
 print('Gabarito do teste 4: 0, 1, 9, 2, 3, 5, 6, 8, 4\n')
 
-# Teste 5
+#Teste 5
 resultados = classifica_teste('testes/teste5.jpeg', modelos)
 print(f'Rede Neural: {resultados[0]}, \nFloresta: {resultados[1]}')
 print('Gabarito do teste 5: 4, 2, 5, 1, 7, 0, 8, 9\n')
